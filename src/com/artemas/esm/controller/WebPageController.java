4cb298950551ed41440c3691ff7073a59d02e99c
Artemas <artemasseng@gmail.com>
2015-09-03 21:24:20 +0100

    Upload of the repo content for the TEST Spring Workspace.

---
+++ src/com/artemas/esm/controller/WebPageController.java
@@ -0,0 +1,14 @@
+package com.artemas.esm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebPageController {
+	
+	@RequestMapping("/")
+	public String showHome(){
+		return "Home";
+	}
+
+}
